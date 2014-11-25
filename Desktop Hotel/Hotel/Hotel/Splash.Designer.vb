5eef0cbd75854d407c72aeb55bf4b78302d6a295
marinagui <marinagui10@hotmail.com>
2014-11-25 11:57:18 -0200

    splash

--- Desktop Hotel/Hotel/Hotel/Splash.Designer.vb
+++ Desktop Hotel/Hotel/Hotel/Splash.Designer.vb
@@ -22,8 +22,79 @@ Partial Class Splash
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Splash))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblMensagem = New System.Windows.Forms.Label()
+        Me.pbProgresso = New System.Windows.Forms.ProgressBar()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Gill Sans MT", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Ivory
+        Me.Label1.Location = New System.Drawing.Point(53, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(468, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Bem-Vindo ao Mountain Ville Hotel"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(75, 58)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(412, 275)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'lblMensagem
+        '
+        Me.lblMensagem.AutoSize = True
+        Me.lblMensagem.Font = New System.Drawing.Font("Gill Sans MT", 15.0!, System.Drawing.FontStyle.Bold)
+        Me.lblMensagem.ForeColor = System.Drawing.Color.Ivory
+        Me.lblMensagem.Location = New System.Drawing.Point(216, 340)
+        Me.lblMensagem.Name = "lblMensagem"
+        Me.lblMensagem.Size = New System.Drawing.Size(75, 29)
+        Me.lblMensagem.TabIndex = 2
+        Me.lblMensagem.Text = "Label2"
+        '
+        'pbProgresso
+        '
+        Me.pbProgresso.Location = New System.Drawing.Point(185, 372)
+        Me.pbProgresso.Name = "pbProgresso"
+        Me.pbProgresso.Size = New System.Drawing.Size(147, 23)
+        Me.pbProgresso.TabIndex = 3
+        '
+        'Splash
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkGray
+        Me.ClientSize = New System.Drawing.Size(564, 418)
+        Me.Controls.Add(Me.pbProgresso)
+        Me.Controls.Add(Me.lblMensagem)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Splash"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Splash"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents lblMensagem As System.Windows.Forms.Label
+    Friend WithEvents pbProgresso As System.Windows.Forms.ProgressBar
 End Class
--- Desktop Hotel/Hotel/Hotel/Splash.vb
+++ Desktop Hotel/Hotel/Hotel/Splash.vb
@@ -1,3 +1,35 @@
-﻿Public Class Splash
+﻿Imports System.Threading
 
+Public Class Splash
+
+    Private Sub Splash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Label1.Show()
+        Me.lblMensagem.Show()
+        Me.PictureBox1.Show()
+        Me.Show()
+        Me.BackColor = Color.FromArgb(205, 220, 205)
+
+    End Sub
+
+    Private Sub Splash_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        lblMensagem.Text = "Carregando dados do sistema..."
+        For i As Integer = 0 To pbProgresso.Maximum / 3
+            pbProgresso.Value = i
+            Thread.Sleep(100)
+        Next
+
+        lblMensagem.Text = "Carregando formulários..."
+        For i = pbProgresso.Maximum / 3 To 2 * (pbProgresso.Maximum / 3)
+            pbProgresso.Value = i
+            Thread.Sleep(100)
+        Next
+
+        lblMensagem.Text = "Carregando banco de dados..."
+        For i = 2 * (pbProgresso.Maximum / 3) To pbProgresso.Maximum
+            pbProgresso.Value = i
+            Thread.Sleep(100)
+        Next
+
+        Thread.Sleep(300)
+    End Sub
 End Class
