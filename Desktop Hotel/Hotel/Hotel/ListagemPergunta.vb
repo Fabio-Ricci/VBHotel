42bd958e4d9ac97b7ddbddbe99d9cba358aac9b8
jovit <joao_vitor_araki_Goncalves@hotmail.com>
2014-11-28 10:42:53 -0200

    Listagem Pergunta Comentado

--- Desktop Hotel/Hotel/Hotel/ListagemPergunta.vb
+++ Desktop Hotel/Hotel/Hotel/ListagemPergunta.vb
@@ -1,7 +1,6 @@
 ﻿Public Class ListagemPergunta
 
-    Private Sub ListagemPergunta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'HosPDeirosDataSet10.hPergunta' table. You can move, or remove it, as needed.
+    Private Sub ListagemPergunta_Load(sender As Object, e As EventArgs) Handles MyBase.Load ' carrega a listagem de pergunta
         Me.HPerguntaTableAdapter.Fill(Me.HosPDeirosDataSet10.hPergunta)
 
     End Sub
